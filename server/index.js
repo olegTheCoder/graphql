9e7a63403524555d8ab8a26a648b2cd3a5b1144b
Oleg Grachev <sk8cool@mail.ru>
2024-02-22 19:04:37 +0300

    Add linters and init graphql schema

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const schema = require('./schema');
+const { graphqlHTTP } = require('express-graphql');
+const PORT = 4000;
+const users = [
+  { id: 1, username: 'Oleg', age: 34 },
+  { id: 2, username: 'Vasya', age: 35 },
+  { id: 3, username: 'Roma', age: 33 },
+];
+
+const app = express();
+
+app.use(cors());
+
+const root = {
+  getAllUsers: () => {
+    return users;
+  },
+  getUser: ({ id }) => {
+    return users.find((user) => user.id === id);
+  },
+};
+
+app.use('/graphql', graphqlHTTP({ graphiql: true, schema, rootValue: root }));
+
+app.listen(PORT, () => console.log(`server is working on port ${PORT}`));
